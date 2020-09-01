2383b4a391d3ba0e7a1b72ff5e443c7a82344583
Jacqueline Williamson <jacqueline_25@hotmail.com>
2020-09-01 09:06:50 -0400

    JavaScript - Too many interruptions when trying to work on this

--- script.js
+++ script.js
@@ -7,56 +7,80 @@ const deleteRecipe = document.querySelector(".deleteRecipe") //button
 
 //const newRecipe = document.getElementById("#newRecipe") 
 
-window.addEventListener('load', function(){
+window.addEventListener('load', function () {
     fetch('http://localhost:3000/notes/')
-    .then(response => response.json())
-    //when the page loads, get a new 'recipe card to type on'
-    .then(recipeCard => {
-    console.log(recipeCard)
-    const recipeCardEls = createNote.map(newNote => createRecipeCardEl(newNote))
-    console.log(recipeCardEls)
-   
+        .then(response => response.json())
+        //when the page loads, get a new 'recipe card to type on'
+        .then(recipeCard => {
+            console.log(recipeCard)
+            const recipeCardEls = createNote.map(newNote => createRecipeCardEl(newNote))
+            console.log(recipeCardEls)
 
-})
-function createRecipeCardEl(newNote){
-    const newNoteEl = document.createElement('div');
-    newNoteEl.id = recipeCard
+            //not sure I need this part yet
+            // for (const newNoteEl of recipeCardsEls){
+            //     a
+            // }
 
-    const noteCardEl = document.createElement('div')
-    div.textContent = recipeCard
-    newNoteEl.appendChild(div)
+        })
+    function createRecipeCardEl(newNote) {
+        const newNoteEl = document.createElement('div');
+        newNoteEl.id = recipeCard
 
-    const bodyEl = document.createElement('div');
-    div.textContent = newNote
-    newNoteEl.appendChild(div)
+        const noteCardEl = document.createElement('card')
+        div.textContent = recipeCard
+        newNoteEl.appendChild(card)
 
-}
-//not sure this function is correct 
+        //note sure if below is necessary
+        //const bodyEl = document.createElement('writing');
+        // div.textContent = newNote
+        // newNoteEl.appendChild(writing)
+//         div = document.createElement('div')
+//         const deleteButton = document.createElement('button')
+//         deleteButton.classList.add
+//         deleteButton.textContent = 'Delete Recipe'
 
-//  function createNote(recipeCard){
-    
-//     for (let i = 0; i < data.length; i++){
-     
-//         const newnoteEl = document.createElement('div');
-//         const notecardEl = document.createElement('card');
-//         const bodyEl = document.createElement('writing');
-//         notecardEl.innerHTML = data[i].card;
-//         bodyEl.innerHTML = data[i].writing;
-//         newnoteEl.appendChild(notecardEl);
-//         bodyEl.appendChild(notecardEl);
-//         console.log(newNote)
-    
-// }
+//         deleteButton.addEventListener('click', function () {
+//             deleteRecipeEntry(recipeId)
+//         }
 
-//event listener//
-//saving the mood
-noteForm.addEventListener('submit', function(event){
-    event.preventDefault()
-    fetch('http://localhost:3000/notes/',{
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ body: newNote.value})
+// function deleteRecipeEntry(recipeId) {
+//                 fetch('http://localhost:3000/notes/' + recipeId, {
+//                     method: 'DELETE'
+//                 })
+//                     .then(res => {
+//                         if (res.status === 200) {
+//                             const recipeCardEl = document.querySelector(recipeId)
+//                             recipeCardEl.remove()
+//                         }
+//                     })
+//             }
+            //not sure this function was correct 
 
-    })}
+            //  function createNote(recipeCard){
 
-)})
+            //     for (let i = 0; i < data.length; i++){
+
+            //         const newnoteEl = document.createElement('div');
+            //         const notecardEl = document.createElement('card');
+            //         const bodyEl = document.createElement('writing');
+            //         notecardEl.innerHTML = data[i].card;
+            //         bodyEl.innerHTML = data[i].writing;
+            //         newnoteEl.appendChild(notecardEl);
+            //         bodyEl.appendChild(notecardEl);
+            //         console.log(newNote)
+
+            // }
+
+            //event listener//
+            //saving the mood
+            noteForm.addEventListener('submit', function (event) {
+                event.preventDefault()
+                fetch('http://localhost:3000/notes/', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({ body: newNote.value })
+
+                })
+            }
+
+            )}})
